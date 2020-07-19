cfb14a809c0e24da4c6b137e0eb23921c64f96e1
ines-tb <ines12tb@gmail.com>
2020-07-19 11:13:55 -0500

    Retiring Employees by Title

--- queries/challenge_queries.sql
+++ queries/challenge_queries.sql
@@ -1,6 +1,10 @@
---Number of Retiring Employees by Title
+-- *******************************************************************************
+-- Technical Analysis Deliverable 1: Number of Retiring Employees by Title
+-- *******************************************************************************
+
+-- Add title, from_date and salary to the current retiring employees
 SELECT ce.emp_no, ce.first_name, ce.last_name, ti.title, de.from_date, sa.salary
-INTO curr_retiring_info
+INTO retirement_curr_info
 FROM current_emp AS ce
 INNER JOIN titles AS ti ON (ce.emp_no = ti.emp_no)
 INNER JOIN salaries AS sa ON (ce.emp_no = sa.emp_no and ti.from_date=sa.from_date)
@@ -13,10 +17,28 @@ WITH identifiedDuplicates AS (
 			PARTITION BY emp_no, first_name, last_name
 			ORDER BY from_date DESC
 		) AS rnum
-	FROM curr_retiring_info
+	FROM retirement_curr_info
 )
 SELECT emp_no, first_name, last_name, title, from_date, salary
-INTO unique_curr_retiring_info
+INTO retirement_curr_info_unique
 FROM identifiedDuplicates 
 WHERE rnum =1
-ORDER BY emp_no
+ORDER BY emp_no;
+
+-- Number of titles retiring
+SELECT count(DISTINCT title) AS titles_count
+-- INTO retirement_no_titles
+FROM retirement_curr_info_unique;
+SELECT * FROM retirement_no_titles;
+
+-- Number of retiring employees with each title
+SELECT COUNT(emp_no) AS emp_count, title
+-- INTO retirement_emp_title
+FROM retirement_curr_info_unique
+GROUP BY title;
+SELECT * FROM retirement_emp_title;
+
+-- List of current employees born between Jan. 1, 1952 and Dec. 31, 1955
+SELECT *
+FROM retirement_curr_info_unique;
+
