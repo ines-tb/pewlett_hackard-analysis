d8e8a5f4e5bfa5e802f1ae31b33ab30e00d3e54f
ines-tb <ines12tb@gmail.com>
2020-07-19 15:29:32 -0500

    Indentation

--- queries/squemas.sql
+++ queries/squemas.sql
@@ -9,17 +9,17 @@ CREATE TABLE departments(
 rollback;
 -- drop table employees
 CREATE TABLE employees (
-	 emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no)
 );
 -- drop table dept_manager
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
@@ -29,12 +29,12 @@ dept_no VARCHAR(4) NOT NULL,
 );
 -- drop table salaries
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
 );
 -- drop table titles;
 CREATE TABLE titles(
@@ -57,4 +57,3 @@ CREATE TABLE dept_employees(
   	PRIMARY KEY (emp_no, dept_no)
 );
 
-select * from dept_employees
